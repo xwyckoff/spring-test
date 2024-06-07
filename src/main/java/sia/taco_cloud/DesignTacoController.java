1c2c605f389f46c1db7b926000fb5ee1b287422d
Xander <xander.wyckoff@gmail.com>
2024-06-06 22:15:12 -0400

    added logic for processing order

--- src/main/java/sia/taco_cloud/DesignTacoController.java
+++ src/main/java/sia/taco_cloud/DesignTacoController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.ui.Model;
@@ -65,6 +66,14 @@ public class DesignTacoController {
             .stream()
             .filter(x -> x.getType().equals(type))
             .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+
+        return "redirect:/orders/current";
     }
 }
 
