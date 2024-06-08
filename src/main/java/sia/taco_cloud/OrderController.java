459b9a85c595ef9e1b95574d2300b1b0ba47f24a
xwyckoff <xander.wyckoff@gmail.com>
2024-06-07 22:50:33 -0400

    added method for handling post request from order form

--- src/main/java/sia/taco_cloud/OrderController.java
+++ src/main/java/sia/taco_cloud/OrderController.java
@@ -2,8 +2,10 @@ package sia.taco_cloud;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,5 +17,12 @@ public class OrderController {
     @GetMapping("/current")
     public String returnOrderPage() {
         return "orderForm";
+    }
+
+    @PostMapping("/order")
+    public String processOrder(TacoOrder order, SessionStatus sessionSatus) {
+        log.info("Order submitted: {}", order);
+        sessionSatus.setComplete();
+        return "redirect:/";
     }
 }
