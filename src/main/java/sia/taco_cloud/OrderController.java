26cb214f0657f4877b8f9a6eb37d4887defcf82d
Xander <xander.wyckoff@gmail.com>
2024-06-10 16:27:05 -0400

    abc

---
+++ src/main/java/sia/taco_cloud/OrderController.java
@@ -0,0 +1,21 @@
+package sia.taco_cloud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import lombok.extern.slf4j.Slf4j;
+import sia.taco_cloud.TacoOrder;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+    
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+}
